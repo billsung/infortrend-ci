117a33727aee81a42c898f3ebee5d2bc8e5fbbcc
billsung <billsung.taiwan@gmail.com>
2017-05-12 14:36:29 +0800

    Fix Tempest Invalid Credentials
    https://review.openstack.org/#/c/263664/
    
    Workaround for Tempest architectural changes
    See bugs:
    1) https://bugs.launchpad.net/manila/+bug/1531049
    2) https://bugs.launchpad.net/tempest/+bug/1524717

--- infortrend-manila-share.sh
+++ infortrend-manila-share.sh
@@ -69,8 +69,22 @@ function pre_test_hook {
 
     sudo chmod 777 $MANILA_CONF
 
-    #source $BASE/new/devstack/openrc admin demo
-    #manila type-key default set driver_handles_share_servers=False
+    TEMPEST_CONFIG=$BASE/new/tempest/etc/tempest.conf
+    ADMIN_TENANT_NAME=${ADMIN_TENANT_NAME:-"admin"}
+    ADMIN_PASSWORD=${ADMIN_PASSWORD:-"secretadmin"}
+    iniset $TEMPEST_CONFIG auth admin_username ${ADMIN_USERNAME:-"admin"}
+    iniset $TEMPEST_CONFIG auth admin_password $ADMIN_PASSWORD
+    iniset $TEMPEST_CONFIG auth admin_tenant_name $ADMIN_TENANT_NAME
+    iniset $TEMPEST_CONFIG auth admin_domain_name ${ADMIN_DOMAIN_NAME:-"Default"}
+    iniset $TEMPEST_CONFIG identity username ${TEMPEST_USERNAME:-"demo"}
+    iniset $TEMPEST_CONFIG identity password $ADMIN_PASSWORD
+    iniset $TEMPEST_CONFIG identity tenant_name ${TEMPEST_TENANT_NAME:-"demo"}
+    iniset $TEMPEST_CONFIG identity alt_username ${ALT_USERNAME:-"alt_demo"}
+    iniset $TEMPEST_CONFIG identity alt_password $ADMIN_PASSWORD
+    iniset $TEMPEST_CONFIG identity alt_tenant_name ${ALT_TENANT_NAME:-"alt_demo"}
+    iniset $TEMPEST_CONFIG validation ip_version_for_ssh 4
+    iniset $TEMPEST_CONFIG validation ssh_timeout $BUILD_TIMEOUT
+    iniset $TEMPEST_CONFIG validation network_for_ssh ${PRIVATE_NETWORK_NAME:-"private"}
 
     cat <<EOF >$BASE/new/devstack/local.conf
 [[local|localrc]]
